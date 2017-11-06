8c6cf2dff0590d03f438ca580ef3a6d5f44df4d8
Gaurav <gaurav.purohit024@gmail.com>
2017-11-06 19:32:55 +0530

    add

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
     var locationFixAchieved : Bool = false
     
     var locmanager = CLLocationManager()
+    var myloc:CLLocation?
+    
+    var myRoute : MKRoute!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,6 +59,7 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
         
          if (locationFixAchieved == false) {
             locationFixAchieved = true
+            myloc = location
             let coregion = MKCoordinateRegionMakeWithDistance((location?.coordinate)!, 1000, 1000)
             myMapView.setRegion(coregion, animated: true)
             
@@ -82,11 +86,11 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
         
         let userLocation = myMapView.userLocation
         
-        if userLocation != nil {
-            let coregion = MKCoordinateRegionMakeWithDistance((userLocation.coordinate), 900, 900)
+//        if userLocation != nil {
+        let coregion = MKCoordinateRegionMakeWithDistance((self.myloc?.coordinate)!, 500, 500)
             
             myMapView.setRegion(coregion, animated: true)
-        }
+//        }
         
         
         
@@ -102,6 +106,51 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
         
     }
     
+    @IBAction func roadmappressed(_ sender: UIButton) {
+        
+        let point1 = MKPointAnnotation()
+        let point2 = MKPointAnnotation()
+        
+        point1.coordinate = CLLocationCoordinate2DMake(25.0305, 121.5360)
+        point1.title = "Taipei"
+        point1.subtitle = "Taiwan"
+        myMapView.addAnnotation(point1)
+        
+        point2.coordinate = CLLocationCoordinate2DMake(24.9511, 121.2358)
+        point2.title = "Chungli"
+        point2.subtitle = "Taiwan"
+        myMapView.addAnnotation(point2)
+        myMapView.centerCoordinate = point2.coordinate
+        
+        myMapView.setRegion(MKCoordinateRegionMake(point2.coordinate, MKCoordinateSpanMake(0.7,0.7)), animated: true)
+        
+        let directionsRequest = MKDirectionsRequest()
+        
+        let markTaipei = MKPlacemark(coordinate: CLLocationCoordinate2DMake(point1.coordinate.latitude, point1.coordinate.longitude), addressDictionary: nil)
+        
+        let markChungli = MKPlacemark(coordinate: CLLocationCoordinate2DMake(point2.coordinate.latitude, point2.coordinate.longitude), addressDictionary: nil)
+        
+        directionsRequest.source = MKMapItem(placemark: markChungli)
+        directionsRequest.destination = MKMapItem(placemark: markTaipei)
+        
+        directionsRequest.transportType = MKDirectionsTransportType.automobile
+        
+        let directions = MKDirections(request: directionsRequest)
+        
+        directions.calculate(completionHandler: {
+            
+            response, error in
+            
+            if error == nil {
+                
+                self.myRoute = response!.routes[0] as MKRoute
+                
+                self.myMapView.add(self.myRoute.polyline)
+                
+            }
+        })
+        
+    }
 }
 
 extension ViewController: MKMapViewDelegate {
@@ -126,6 +175,31 @@ extension ViewController: MKMapViewDelegate {
     //    return view
     //  }
   
+        func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+            print("selected \(String(describing: view.annotation?.coordinate)) ff \(String(describing: view.annotation?.title))")
+            
+    
+    }
+    
+    func mapView(_ mapView: MKMapView,
+                 rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        
+        let polylineRenderer = MKPolylineRenderer(overlay: overlay)
+        if (overlay is MKPolyline) {
+            if mapView.overlays.count == 1 {
+                polylineRenderer.strokeColor =
+                    UIColor.blue.withAlphaComponent(0.75)
+            } else if mapView.overlays.count == 2 {
+                polylineRenderer.strokeColor =
+                    UIColor.green.withAlphaComponent(0.75)
+            } else if mapView.overlays.count == 3 {
+                polylineRenderer.strokeColor =
+                    UIColor.red.withAlphaComponent(0.75)
+            }
+            polylineRenderer.lineWidth = 5
+        }
+        return polylineRenderer
+    }
     
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView,
                  calloutAccessoryControlTapped control: UIControl) {
